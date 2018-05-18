f0cb4df251aa3819e1ad38497d6b19542d4a7812
ppabc <ppabc@qq.com>
2018-05-18 17:15:52 +0800

    update

--- mysql_slave2/startup.sh
+++ mysql_slave2/startup.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-if [ -d /data/mysql ]; then
+  MYSQL_DATABASE=${MYSQL_DATABASE:-""}
+  MYSQL_USER=${MYSQL_USER:-""}
+  MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
+
+if [ -d /data2/mysql ]; then
   echo "[i] MySQL directory already present, skipping creation"
 else
   echo "[i] MySQL data directory not found, creating initial DBs"
-  mkdir -p /data/mysql/ /data/mysql/data/ /data/mysql/logs/
-  chown -R root:mysql /data/mysql/
-  mysql_install_db --user=root --datadir=/data/mysql/data/ > /dev/null
+  mkdir -p /data2/mysql/ /data2/mysql/data/ /data2/mysql/logs/
+  chown -R root:mysql /data2/mysql/
+  mysql_install_db --user=root --datadir=/data2/mysql/data/ > /dev/null
   
   if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
     MYSQL_ROOT_PASSWORD=111111
     echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
   fi
-
-  MYSQL_DATABASE=${MYSQL_DATABASE:-""}
-  MYSQL_USER=${MYSQL_USER:-""}
-  MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
 
   if [ ! -d "/run/mysqld" ]; then
     mkdir -p /run/mysqld
@@ -30,9 +30,10 @@ else
   cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root' AND host='localhost';
+UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root' AND host='127.0.0.1';
 EOF
 
   if [ "$MYSQL_DATABASE" != "" ]; then
@@ -45,8 +46,8 @@ EOF
     fi
   fi
 
-  /usr/bin/mysqld --user=root --bootstrap --datadir=/data/mysql/data/ --verbose=0 < $tfile
+  /usr/bin/mysqld --user=root --bootstrap --datadir=/data2/mysql/data/ --verbose=0 < $tfile
   rm -f $tfile
 fi
 
-exec /usr/bin/mysqld --user=root --datadir=/data/mysql/data --console
+exec /usr/bin/mysqld --user=root --datadir=/data2/mysql/data --console
